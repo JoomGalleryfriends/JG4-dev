4a7583ecb5b79766bdf3612f1fc5dd13649a2611
19leunam93 <tech.spuur@quickline.ch>
2022-07-27 16:21:55 +0200

    update images view

--- administrator/com_joomgallery/src/View/Images/HtmlView.php
+++ administrator/com_joomgallery/src/View/Images/HtmlView.php
@@ -51,7 +51,7 @@ class HtmlView extends JoomGalleryView
 	public function display($tpl = null)
 	{
 		$this->items = $this->get('Items');
-    	$this->state = $this->get('State');
+    $this->state = $this->get('State');
 		$this->pagination = $this->get('Pagination');
 		$this->filterForm = $this->get('FilterForm');
 		$this->activeFilters = $this->get('ActiveFilters');
@@ -78,7 +78,7 @@ class HtmlView extends JoomGalleryView
 	protected function addToolbar()
 	{
 		$state = $this->get('State');
-		$canDo = JoomHelper::getActions();
+		$canDo = JoomHelper::getActions('image');
 
 		ToolbarHelper::title(Text::_('COM_JOOMGALLERY_IMAGE_MANAGER'), "image");
 
@@ -95,55 +95,57 @@ class HtmlView extends JoomGalleryView
 			}
 		}
 
+    if($canDo->get('core.delete'))
+    {
+      $toolbar->delete('images.delete')
+        ->text('JTOOLBAR_DELETE')
+        ->message(Text::_('COM_JOOMGALLERY_CONFIRM_DELETE_IMAGES'))
+        ->listCheck(true);
+    }
+
 		if($canDo->get('core.edit.state')  || count($this->transitions))
 		{
 			$dropdown = $toolbar->dropdownButton('status-group')
-				->text('JTOOLBAR_CHANGE_STATUS')
+				->text('JTOOLBAR_PUBLISH')
 				->toggleSplit(false)
 				->icon('fas fa-ellipsis-h')
 				->buttonClass('btn btn-action')
 				->listCheck(true);
 
-			$childBar = $dropdown->getChildToolbar();
+			$status_childBar = $dropdown->getChildToolbar();
 
-			if(isset($this->items[0]->state))
+			if(isset($this->items[0]->published))
 			{
-				$childBar->publish('images.publish')->listCheck(true);
-				$childBar->unpublish('images.unpublish')->listCheck(true);
-				$childBar->archive('images.archive')->listCheck(true);
-			}
-			elseif(isset($this->items[0]))
-			{
-				// If this component does not use state then show a direct delete button as we can not trash
-				$toolbar->delete('images.delete')
-				->text('JTOOLBAR_EMPTY_TRASH')
-				->message('JGLOBAL_CONFIRM_DELETE')
-				->listCheck(true);
+				$status_childBar->publish('images.publish')->listCheck(true);
+				$status_childBar->unpublish('images.unpublish')->listCheck(true);
 			}
 
-			$childBar->standardButton('duplicate')
-				->text('JTOOLBAR_DUPLICATE')
-				->icon('fas fa-copy')
-				->task('images.duplicate')
-				->listCheck(true);
+      if($canDo->get('core.edit'))
+      {
+        $batch_dropdown = $toolbar->dropdownButton('batch-group')
+          ->text('JTOOLBAR_BATCH')
+          ->toggleSplit(false)
+          ->icon('fas fa-ellipsis-h')
+          ->buttonClass('btn btn-action')
+          ->listCheck(true);
+        
+        $batch_childBar = $batch_dropdown->getChildToolbar();
 
-			if(isset($this->items[0]->checked_out))
-			{
-				$childBar->checkin('images.checkin')->listCheck(true);
-			}
-
-			if(isset($this->items[0]->state))
-			{
-				$childBar->trash('images.trash')->listCheck(true);
-			}
+        // Duplicate button inside batch dropdown
+        $batch_childBar->standardButton('duplicate')
+          ->text('JTOOLBAR_DUPLICATE')
+          ->icon('fas fa-copy')
+          ->task('images.duplicate')
+          ->listCheck(true);
+      }
 		}
 
 		// Show trash and delete for components that uses the state field
-		if(isset($this->items[0]->state))
+		if(isset($this->items[0]->published))
 		{
-			if($this->state->get('filter.state') == ContentComponent::CONDITION_TRASHED && $canDo->get('core.delete'))
+			if($this->state->get('filter.published') == ContentComponent::CONDITION_TRASHED && $canDo->get('core.delete'))
 			{
-				$toolbar->delete('images.delete')
+				$toolbar->delete('categories.delete')
 					->text('JTOOLBAR_EMPTY_TRASH')
 					->message('JGLOBAL_CONFIRM_DELETE')
 					->listCheck(true);
@@ -167,19 +169,19 @@ class HtmlView extends JoomGalleryView
 	protected function getSortFields()
 	{
 		return array(
-			'a.`ordering`' => Text::_('JGRID_HEADING_ORDERING'),
-			'a.`hits`' => Text::_('COM_JOOMGALLERY_COMMON_HITS'),
-			'a.`downloads`' => Text::_('COM_JOOMGALLERY_COMMON_DOWNLOADS'),
-			'a.`approved`' => Text::_('COM_JOOMGALLERY_COMMON_APPROVED'),
-			'a.`imgtitle`' => Text::_('JGLOBAL_TITLE'),
-			'a.`catid`' => Text::_('JCATEGORY'),
-			'a.`published`' => Text::_('JSTATUS'),
-			'a.`imgauthor`' => Text::_('JAUTHOR'),
-			'a.`language`' => Text::_('JGRID_HEADING_LANGUAGE'),
-			'a.`access`' => Text::_('JGRID_HEADING_ACCESS'),
+			'a.`ordering`'   => Text::_('JGRID_HEADING_ORDERING'),
+			'a.`hits`'       => Text::_('COM_JOOMGALLERY_COMMON_HITS'),
+			'a.`downloads`'  => Text::_('COM_JOOMGALLERY_COMMON_DOWNLOADS'),
+			'a.`approved`'   => Text::_('COM_JOOMGALLERY_COMMON_APPROVED'),
+			'a.`imgtitle`'   => Text::_('JGLOBAL_TITLE'),
+			'a.`catid`'      => Text::_('JCATEGORY'),
+			'a.`published`'  => Text::_('JSTATUS'),
+			'a.`imgauthor`'  => Text::_('JAUTHOR'),
+			'a.`language`'   => Text::_('JGRID_HEADING_LANGUAGE'),
+			'a.`access`'     => Text::_('JGRID_HEADING_ACCESS'),
 			'a.`created_by`' => Text::_('JGLOBAL_FIELD_CREATED_BY_LABEL'),
-			'a.`id`' => Text::_('JGRID_HEADING_ID'),
-			'a.`imgdate`' => Text::_('JDATE'),
+			'a.`id`'         => Text::_('JGRID_HEADING_ID'),
+			'a.`imgdate`'    => Text::_('JDATE'),
 		);
 	}
 
--- administrator/com_joomgallery/tmpl/categories/default.php
+++ administrator/com_joomgallery/tmpl/categories/default.php
@@ -52,173 +52,175 @@ if ($saveOrder && !empty($this->items))
 			<div id="j-main-container" class="j-main-container">
 				<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 				<div class="clearfix"></div>
-				<table class="table table-striped" id="categoryList">
-					<thead>
-						<tr>
-              <td class="w-1 text-center">
-									<?php echo HTMLHelper::_('grid.checkall'); ?>
-								</td>
-							<?php if (isset($this->items[0]->ordering)): ?>
-								<th scope="col" class="w-1 text-center d-none d-md-table-cell">
-									<?php echo HTMLHelper::_('searchtools.sort', '', 'a.ordering', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-menu-2'); ?>
-								</th>
-							<?php endif; ?>
-							<th scope="col" class="w-1 text-center">
-								<?php echo HTMLHelper::_('searchtools.sort', '', 'a.lft', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-sort'); ?>
-							</th>
-							<th scope="col" class="w-1 text-center">
-                <?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
-              </th>
-							<th scope="col" style="min-width:100px">
-								<?php echo HTMLHelper::_('searchtools.sort',  'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
-							</th>
-              <th scope="col" class="w-10 d-none d-md-table-cell">
-                <?php echo HTMLHelper::_('searchtools.sort',  'COM_JOOMGALLERY_COMMON_PARENT_CATEGORY', 'a.parent_id', $listDirn, $listOrder); ?>
-							</th>
-              <th scope="col" class="w-10 d-none d-md-table-cell">
-								<?php echo Text::_('Images'); ?>
-							</th>
-							<th scope="col" class="w-10 d-none d-md-table-cell">
-								<?php echo HTMLHelper::_('searchtools.sort',  'JGRID_HEADING_ACCESS', 'a.access', $listDirn, $listOrder); ?>
-							</th>
-							<?php if (Multilanguage::isEnabled()) : ?>
-								<th scope="col" class="w-10 d-none d-md-table-cell">
-									<?php echo HTMLHelper::_('searchtools.sort',  'JGRID_HEADING_LANGUAGE', 'a.language', $listDirn, $listOrder); ?>
-								</th>
-							<?php endif; ?>
-							<th scope="col" class="w-10 d-none d-md-table-cell">
-								<?php echo HTMLHelper::_('searchtools.sort',  'COM_JOOMGALLERY_COMMON_OWNER', 'a.created_by', $listDirn, $listOrder); ?>
-							</th>
-							<th scope="col" class="w-3 d-none d-lg-table-cell"> 
-								<?php echo HTMLHelper::_('searchtools.sort',  'JGLOBAL_FIELD_ID_LABEL', 'a.id', $listDirn, $listOrder); ?>
-							</th>
-						</tr>
-					</thead>
-					<tfoot>
-						<tr>
-							<td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
-								<?php echo $this->pagination->getListFooter(); ?>
-							</td>
-						</tr>
-					</tfoot>
-					<tbody <?php if ($saveOrder) :?> class="js-draggable" data-url="<?php echo $saveOrderingUrl; ?>" data-direction="<?php echo strtolower($listDirn); ?>" <?php endif; ?>>
-						<?php foreach ($this->items as $i => $item) :
-							$ordering   = ($listOrder == 'a.ordering');
-							$canCreate  = $user->authorise('core.create', _JOOM_OPTION.'.category.'.$item->id);
-							$canEdit    = $user->authorise('core.edit', _JOOM_OPTION.'.category.'.$item->id);
-              $canEditOwn = $user->authorise('core.edit.own', _JOOM_OPTION.'.category.'.$item->id) && $item->created_by == $userId;
-							$canCheckin = $user->authorise('core.manage', _JOOM_OPTION.'.category.'.$item->id);
-							$canChange  = $user->authorise('core.edit.state', _JOOM_OPTION.'.category.'.$item->id);
-							?>
-						<tr class="row<?php echo $i % 2; ?>">
-							<?php if (isset($this->items[0]->ordering)) : ?>
-								<td class="order nowrap center hidden-phone">
-									<?php
-										$iconClass = '';
-										if (!$canChange)
-										{
-											$iconClass = ' inactive';
-										}
-										elseif (!$saveOrder)
-										{
-											$iconClass = ' inactive" title="' . Text::_('JORDERINGDISABLED');
-										}
-									?>
-									<span class="sortable-handler<?php echo $iconClass ?>">
-										<span class="icon-ellipsis-v" aria-hidden="true"></span>
-									</span>
-									<?php if ($canChange && $saveOrder) : ?>
-										<input type="text" name="order[]" size="5" value="<?php echo $item->ordering; ?>" class="width-20 text-area-order hidden">
-									<?php endif; ?>
-								</td>
-							<?php endif; ?>
-
-              <td class="text-center">
-                <?php echo HTMLHelper::_('grid.id', $i, $item->id, false, 'cid', 'cb', $item->title); ?>
-              </td>
-
-              <td class="text-center d-none d-md-table-cell">
-								<?php
-								$iconClass = '';
-								if (!$canChange)
-								{
-								$iconClass = ' inactive';
-								}
-								elseif (!$saveOrder)
-								{
-								$iconClass = ' inactive" title="' . Text::_('JORDERINGDISABLED');
-								}
-								?>
-								<span class="sortable-handler<?php echo $iconClass ?>">
-								<span class="icon-ellipsis-v"></span>
-								</span>
-								<?php if ($canChange && $saveOrder) : ?>
-								  <input type="text" name="order[]" size="5" value="<?php echo $item->lft; ?>" class="width-20 text-area-order hidden">
-								<?php endif; ?>
-							</td>
-
-							<td class="category-status text-center">
-                <?php 
-                  $options = [
-                    'task_prefix' => 'categories.',
-                    'disabled' => !$canChange,
-                    'id' => 'state-' . $item->id
-                  ];
-
-                  echo (new PublishedButton)->render((int) $item->published, $i, $options); 
+        <div class="table-responsive">
+          <table class="table table-striped" id="categoryList">
+            <thead>
+              <tr>
+                <td class="w-1 text-center">
+                    <?php echo HTMLHelper::_('grid.checkall'); ?>
+                  </td>
+                <?php if (isset($this->items[0]->ordering)): ?>
+                  <th scope="col" class="w-1 text-center d-none d-md-table-cell">
+                    <?php echo HTMLHelper::_('searchtools.sort', '', 'a.ordering', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-menu-2'); ?>
+                  </th>
+                <?php endif; ?>
+                <th scope="col" class="w-1 text-center">
+                  <?php echo HTMLHelper::_('searchtools.sort', '', 'a.lft', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-sort'); ?>
+                </th>
+                <th scope="col" class="w-1 text-center">
+                  <?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
+                </th>
+                <th scope="col" style="min-width:100px">
+                  <?php echo HTMLHelper::_('searchtools.sort',  'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+                </th>
+                <th scope="col" class="w-10 d-none d-md-table-cell">
+                  <?php echo HTMLHelper::_('searchtools.sort',  'COM_JOOMGALLERY_COMMON_PARENT_CATEGORY', 'a.parent_id', $listDirn, $listOrder); ?>
+                </th>
+                <th scope="col" class="w-10 d-none d-md-table-cell">
+                  <?php echo Text::_('Images'); ?>
+                </th>
+                <th scope="col" class="w-10 d-none d-md-table-cell">
+                  <?php echo HTMLHelper::_('searchtools.sort',  'JGRID_HEADING_ACCESS', 'a.access', $listDirn, $listOrder); ?>
+                </th>
+                <?php if (Multilanguage::isEnabled()) : ?>
+                  <th scope="col" class="w-10 d-none d-md-table-cell">
+                    <?php echo HTMLHelper::_('searchtools.sort',  'JGRID_HEADING_LANGUAGE', 'a.language', $listDirn, $listOrder); ?>
+                  </th>
+                <?php endif; ?>
+                <th scope="col" class="w-10 d-none d-md-table-cell">
+                  <?php echo HTMLHelper::_('searchtools.sort',  'COM_JOOMGALLERY_COMMON_OWNER', 'a.created_by', $listDirn, $listOrder); ?>
+                </th>
+                <th scope="col" class="w-3 d-none d-lg-table-cell"> 
+                  <?php echo HTMLHelper::_('searchtools.sort',  'JGLOBAL_FIELD_ID_LABEL', 'a.id', $listDirn, $listOrder); ?>
+                </th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
+                  <?php echo $this->pagination->getListFooter(); ?>
+                </td>
+              </tr>
+            </tfoot>
+            <tbody <?php if ($saveOrder) :?> class="js-draggable" data-url="<?php echo $saveOrderingUrl; ?>" data-direction="<?php echo strtolower($listDirn); ?>" <?php endif; ?>>
+              <?php foreach ($this->items as $i => $item) :
+                $ordering   = ($listOrder == 'a.ordering');
+                $canCreate  = $user->authorise('core.create', _JOOM_OPTION.'.category.'.$item->id);
+                $canEdit    = $user->authorise('core.edit', _JOOM_OPTION.'.category.'.$item->id);
+                $canEditOwn = $user->authorise('core.edit.own', _JOOM_OPTION.'.category.'.$item->id) && $item->created_by == $userId;
+                $canCheckin = $user->authorise('core.manage', _JOOM_OPTION.'.category.'.$item->id);
+                $canChange  = $user->authorise('core.edit.state', _JOOM_OPTION.'.category.'.$item->id);
                 ?>
-							</td>
+              <tr class="row<?php echo $i % 2; ?>">
+                <?php if (isset($this->items[0]->ordering)) : ?>
+                  <td class="order nowrap center hidden-phone">
+                    <?php
+                      $iconClass = '';
+                      if (!$canChange)
+                      {
+                        $iconClass = ' inactive';
+                      }
+                      elseif (!$saveOrder)
+                      {
+                        $iconClass = ' inactive" title="' . Text::_('JORDERINGDISABLED');
+                      }
+                    ?>
+                    <span class="sortable-handler<?php echo $iconClass ?>">
+                      <span class="icon-ellipsis-v" aria-hidden="true"></span>
+                    </span>
+                    <?php if ($canChange && $saveOrder) : ?>
+                      <input type="text" name="order[]" size="5" value="<?php echo $item->ordering; ?>" class="width-20 text-area-order hidden">
+                    <?php endif; ?>
+                  </td>
+                <?php endif; ?>
 
-							<th scope="row" class="has-context">
-                <div class="break-word">
-                  <?php echo LayoutHelper::render('joomla.html.treeprefix', array('level' => $item->level)); ?>
-                  <?php if (isset($item->checked_out) && $item->checked_out && ($canEdit || $canChange)) : ?>
-                    <?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->uEditor, $item->checked_out_time, 'categories.', $canCheckin); ?>
+                <td class="text-center">
+                  <?php echo HTMLHelper::_('grid.id', $i, $item->id, false, 'cid', 'cb', $item->title); ?>
+                </td>
+
+                <td class="text-center d-none d-md-table-cell">
+                  <?php
+                  $iconClass = '';
+                  if (!$canChange)
+                  {
+                  $iconClass = ' inactive';
+                  }
+                  elseif (!$saveOrder)
+                  {
+                  $iconClass = ' inactive" title="' . Text::_('JORDERINGDISABLED');
+                  }
+                  ?>
+                  <span class="sortable-handler<?php echo $iconClass ?>">
+                  <span class="icon-ellipsis-v"></span>
+                  </span>
+                  <?php if ($canChange && $saveOrder) : ?>
+                    <input type="text" name="order[]" size="5" value="<?php echo $item->lft; ?>" class="width-20 text-area-order hidden">
                   <?php endif; ?>
-                  <?php if ($canEdit || $canEditOwn) : ?>
-                    <a href="<?php echo Route::_('index.php?option=com_joomgallery&task=category.edit&id='.(int) $item->id); ?>">
+                </td>
+
+                <td class="category-status text-center">
+                  <?php 
+                    $options = [
+                      'task_prefix' => 'categories.',
+                      'disabled' => !$canChange,
+                      'id' => 'state-' . $item->id
+                    ];
+
+                    echo (new PublishedButton)->render((int) $item->published, $i, $options); 
+                  ?>
+                </td>
+
+                <th scope="row" class="has-context">
+                  <div class="break-word">
+                    <?php echo LayoutHelper::render('joomla.html.treeprefix', array('level' => $item->level)); ?>
+                    <?php if (isset($item->checked_out) && $item->checked_out && ($canEdit || $canChange)) : ?>
+                      <?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->uEditor, $item->checked_out_time, 'categories.', $canCheckin); ?>
+                    <?php endif; ?>
+                    <?php if ($canEdit || $canEditOwn) : ?>
+                      <a href="<?php echo Route::_('index.php?option=com_joomgallery&task=category.edit&id='.(int) $item->id); ?>">
+                        <?php echo $this->escape($item->title); ?>
+                      </a>
+                    <?php else : ?>
                       <?php echo $this->escape($item->title); ?>
-                    </a>
-                  <?php else : ?>
-                    <?php echo $this->escape($item->title); ?>
-                  <?php endif; ?>
-                  <div class="small break-word">
-                      <?php echo LayoutHelper::render('joomla.html.treeprefix', array('level' => $item->level)); ?>
-                      <?php echo Text::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias)); ?>
+                    <?php endif; ?>
+                    <div class="small break-word">
+                        <?php echo LayoutHelper::render('joomla.html.treeprefix', array('level' => $item->level)); ?>
+                        <?php echo Text::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias)); ?>
+                    </div>
                   </div>
-                </div>
-							</th>
+                </th>
 
-              <td class="d-none d-md-table-cell">
-                <?php echo ($item->parent_title == 'Root') ? '' : $item->parent_title; ?>
-							</td>
+                <td class="d-none d-md-table-cell">
+                  <?php echo ($item->parent_title == 'Root') ? '' : $item->parent_title; ?>
+                </td>
 
-              <td class="d-none d-md-table-cell">
-              <?php if($item->img_count > 0) : ?>
-                <a href="<?php echo JRoute::_('index.php?option='._JOOM_OPTION.'&view=images&filter[category]='.$item->id); ?>">(<?php echo $item->img_count; ?>)</a>
-              <?php else : ?>
-                (0)
-              <?php endif; ?>
-							</td>
+                <td class="d-none d-md-table-cell">
+                <?php if($item->img_count > 0) : ?>
+                  <a href="<?php echo JRoute::_('index.php?option='._JOOM_OPTION.'&view=images&filter[category]='.$item->id); ?>">(<?php echo $item->img_count; ?>)</a>
+                <?php else : ?>
+                  (0)
+                <?php endif; ?>
+                </td>
 
-							<td class="small d-none d-md-table-cell">
-								<?php echo $item->access; ?>
-							</td>
-							<?php if (Multilanguage::isEnabled()) : ?>
-								<td class="small d-none d-md-table-cell">
-								<?php echo $item->language; ?>
-								</td>
-							<?php endif; ?>
-							<td class="small d-none d-md-table-cell">
-								<?php echo $item->created_by; ?>
-							</td>
-							<td>
-								<?php echo $item->id; ?>
-							</td>
-						</tr>
-						<?php endforeach; ?>
-					</tbody>
-				</table>
+                <td class="small d-none d-md-table-cell">
+                  <?php echo $item->access; ?>
+                </td>
+                <?php if (Multilanguage::isEnabled()) : ?>
+                  <td class="small d-none d-md-table-cell">
+                  <?php echo $item->language; ?>
+                  </td>
+                <?php endif; ?>
+                <td class="small d-none d-md-table-cell">
+                  <?php echo $item->created_by; ?>
+                </td>
+                <td>
+                  <?php echo $item->id; ?>
+                </td>
+              </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
+        </div>
 				<input type="hidden" name="task" value=""/>
 				<input type="hidden" name="boxchecked" value="0"/>
         <input type="hidden" id="del_force" name="del_force" value="0"/>
