213efc796d185a10d26614dc656a6b9ec0b1b97e
19leunam93 <tech.spuur@quickline.ch>
2022-11-28 18:00:11 +0100

    sensitive fields without subforms

--- administrator/com_joomgallery/layouts/joomla/form/renderfield.php
+++ administrator/com_joomgallery/layouts/joomla/form/renderfield.php
@@ -38,7 +38,13 @@ if(isset($id) && ($id == 'jform_jg_staticprocessing' || $id == 'jform_jg_dynamic
   $addClass = 'mb-5';
 }
 
+if(strpos($id, 'jg_imgtypepath') !== false)
+{
+  $tmp = true;
+}
+
 $unused          = (isset($class) && strpos($class, 'unused') !== false) ? 'unused' : '';
+$sensitive       = (!empty($options['sensitive']) && $options['sensitive'] == true) ? true : false;
 $class           = empty($options['class']) ? '' : ' ' . $options['class'];
 $rel             = empty($options['rel']) ? '' : ' ' . $options['rel'];
 $id              = ($id ?? $name) . '-desc';
