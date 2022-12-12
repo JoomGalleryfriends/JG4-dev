02a421f6ce3894773010d992750657d90f886d55
19leunam93 <tech.spuur@quickline.ch>
2022-12-12 16:46:35 +0100

    add contenttypes to JoomHelper

--- administrator/com_joomgallery/src/Helper/JoomHelper.php
+++ administrator/com_joomgallery/src/Helper/JoomHelper.php
@@ -35,7 +35,7 @@ class JoomHelper
    *
    * @var array
    */
-  protected static $content_types = array('category', 'config', 'image', 'imagetype', 'tag');
+  protected static $content_types = array('category', 'config', 'field', 'image', 'imagetype', 'tag', 'user', 'vote');
 
   /**
 	 * Gets the JoomGallery component object
