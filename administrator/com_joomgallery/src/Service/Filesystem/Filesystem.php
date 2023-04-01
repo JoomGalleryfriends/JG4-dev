e118973ff0e984a1a6fed4162060796aa93a3066
19leunam93 <tech.spuur@quickline.ch>
2023-04-01 23:43:56 +0200

    update cleanFilename

--- administrator/com_joomgallery/src/Service/Filesystem/Filesystem.php
+++ administrator/com_joomgallery/src/Service/Filesystem/Filesystem.php
@@ -17,6 +17,7 @@ use \Joomla\CMS\Factory;
 use \Joomla\CMS\Language\Text;
 use \Joomla\CMS\Object\CMSObject;
 use \Joomla\CMS\Plugin\PluginHelper;
+use \Joomla\CMS\Filter\OutputFilter;
 use \Joomla\CMS\Filesystem\File as JFile;
 use \Joomla\CMS\Filesystem\Path as JPath;
 
@@ -123,116 +124,22 @@ class Filesystem implements AdapterInterface, FilesystemInterface
    * optionally replace extension if present
    * replace special chars defined in the configuration
    *
-   * @param   string    $file            The file name
-   * @param   integer   $with_ext        0: strip extension, 1: force extension, 2: leave it as it is (default: 2)
-   * @param   string    $use_ext         Extension to use if $file given without extension
-   * @param   string    $replace_chars   Characters to be replaced
+   * @param   string    $file        The file name
+   * @param   integer   $with_ext    0: strip extension, 1: force extension, 2: leave it as it is (default: 2)
+   * @param   string    $def_ext     Extension to use if $file given without extension
    *
    * @return  mixed     cleaned name on success, false otherwise
    *
    * @since   4.0.0
    */
-  public function cleanFilename(string $file, int $with_ext=2, string $use_ext='jpg', string $replace_chars='')
+  public function cleanFilename(string $file, int $with_ext=2, string $def_ext='jpg')
   {
-    // Check if multibyte support installed
-    if(\in_array ('mbstring', \get_loaded_extensions()))
-    {
-      // Get the funcs from mb
-      $funcs = \get_extension_funcs('mbstring');
-      if(\in_array ('mb_detect_encoding', $funcs) && \in_array ('mb_strtolower', $funcs))
-      {
-        // Try to check if the name contains UTF-8 characters
-        $isUTF = \mb_detect_encoding($file, 'UTF-8', true);
-        if($isUTF)
-        {
-          // Try to lower the UTF-8 characters
-          $file = \mb_strtolower($file, 'UTF-8');
-        }
-        else
-        {
-          // Try to lower the one byte characters
-          $file = \strtolower($file);
-        }
-      }
-      else
-      {
-        // TODO mbstring loaded but no needed functions
-        // --> server misconfiguration
-        $file = \strtolower($file);
-      }
-    }
-    else
-    {
-      // TODO no mbstring loaded, appropriate server for Joomla?
-      $file = \strtolower($file);
-    }
-
-    // Replace special chars
-    $filenamesearch  = array();
-    $filenamereplace = array();
-
-    $items = \explode(',', $replace_chars);
-    if($items != false)
-    {
-      // Contains pairs of <specialchar>|<replaced char(s)>
-      foreach($items as $item)
-      {
-        if(!empty($item))
-        {
-          $workarray = \explode('|', \trim($item));
-          if($workarray != false && isset($workarray[0]) && !empty($workarray[0]) && isset($workarray[1]) && !empty($workarray[1]))
-          {
-            \array_push($filenamesearch, \preg_quote($workarray[0]));
-            \array_push($filenamereplace, \preg_quote($workarray[1]));
-          }
-        }
-      }
-    }
-
-    // Replace whitespace with underscore
-    \array_push($filenamesearch, '\s');
-    \array_push($filenamereplace, '_');
-    // Replace slash with underscore
-    \array_push($filenamesearch, '/');
-    \array_push($filenamereplace, '_');
-    // Replace backslash with underscore
-    \array_push($filenamesearch, '\\\\');
-    \array_push($filenamereplace, '_');
-    // Replace other stuff
-    \array_push($filenamesearch, '[^a-z_0-9-]');
-    \array_push($filenamereplace, '');
-
-    // Checks for different array-length
-    $lengthsearch  = \count($filenamesearch);
-    $lengthreplace = \count($filenamereplace);
-    if($lengthsearch > $lengthreplace)
-    {
-      while($lengthsearch > $lengthreplace)
-      {
-        \array_push($filenamereplace, '');
-        $lengthreplace = $lengthreplace + 1;
-      }
-    }
-    else
-    {
-      if($lengthreplace > $lengthsearch)
-      {
-        while($lengthreplace > $lengthsearch)
-        {
-          \array_push($filenamesearch, '');
-          $lengthsearch = $lengthsearch + 1;
-        }
-      }
-    }
-
-    $detect_ext = JFile::getExt($file);
+    $ext = JFile::getExt($file);
 
     // Replace extension if present
-    if($detect_ext)
+    if($ext)
     {
-      $fileextensionlength  = \strlen($detect_ext);
-      $filenamelength       = \strlen($file);
-      $filename             = \substr($file, -$filenamelength, -$fileextensionlength - 1);
+      $filename = \substr($file, 0, -\strlen($ext) - 1);
     }
     else
     {
@@ -240,95 +147,42 @@ class Filesystem implements AdapterInterface, FilesystemInterface
       $filename = $file;
     }
 
-    // Perform the replace
-    for($i = 0; $i < $lengthreplace; $i++)
+    if(Factory::getConfig()->get('unicodeslugs') == 1)
     {
-      $searchstring = '!'.$filenamesearch[$i].'+!i';
-      $filename     = \preg_replace($searchstring, $filenamereplace[$i], $filename);
+      $filename = OutputFilter::stringURLUnicodeSlug(trim($filename));
+    }
+    else
+    {
+      $filename = OutputFilter::stringURLSafe(trim($filename));
     }
 
-    switch($with_ext)
+    switch ($with_ext)
     {
       case 0:
         // strip extension
         break;
 
       case 1:
-        // add extension
-        if($detect_ext)
+        // force extension
+        if($ext)
         {
-          $filename = $filename.'.'. \strtolower($detect_ext);
+          $filename = $filename.'.'. \strtolower($ext);
         }
         else
         {
-          $filename = $filename.'.'. \strtolower($use_ext);
+          $filename = $filename.'.'. \strtolower($def_ext);
         }
         break;
       
       default:
-        // leave it as it is
-        if($detect_ext)
+        if($ext)
         {
-          $filename = $filename.'.'. \strtolower($detect_ext);
+          $filename = $filename.'.'. \strtolower($ext);
         }
         break;
     }
 
     return $filename;
-  }
-
-  /**
-   * Check filename if it's valid for the filesystem
-   *
-   * @param   string    $nameb          filename before any processing
-   * @param   string    $namea          filename after processing in e.g. fixFilename
-   * @param   bool      $checkspecial   True if the filename shall be checked for special characters only
-   *
-   * @return  bool      True if the filename is valid, false otherwise
-   *
-   * @since   4.0.0
-  */
-  public function checkFilename(string $nameb, string $namea = '', bool $checkspecial = false): bool
-  {
-    // TODO delete this function and the call of them?
-    // return true;
-
-    // Check only for special characters
-    if($checkspecial)
-    {
-      $pattern = '/[^0-9A-Za-z -_]/';
-      $check = \preg_match($pattern, $nameb);
-      if($check == 0)
-      {
-        // No special characters found
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-    }
-    // Remove extension from names
-    $nameb = JFile::stripExt($nameb);
-    $namea = JFile::stripExt($namea);
-
-    // Check the old filename for containing only underscores
-    if(\strlen($nameb) - \substr_count($nameb, '_') == 0)
-    {
-      $nameb_onlyus = true;
-    }
-    else
-    {
-      $nameb_onlyus = false;
-    }
-    if(empty($namea) || (!$nameb_onlyus && strlen($namea) == substr_count($nameb, '_')))
-    {
-      return false;
-    }
-    else
-    {
-      return true;
-    }
   }
 
   /**
