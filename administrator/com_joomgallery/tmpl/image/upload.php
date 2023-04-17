68eaa9d8d70c405a8d7f3b702ee6993a5cc252ac
19leunam93 <tech.spuur@quickline.ch>
2023-04-17 19:16:26 +0200

    locally host uppy assets

--- administrator/com_joomgallery/tmpl/image/upload.php
+++ administrator/com_joomgallery/tmpl/image/upload.php
@@ -18,15 +18,18 @@ use \Joomla\CMS\Uri\Uri;
 use \Joomla\CMS\Router\Route;
 use \Joomla\CMS\Language\Text;
 
-// Uppy config
-$uppy_version = 'v3.5.0'; // Uppy version to use
+$uppy_version = '3.7.0'; // Uppy version to use
+$path_css = Uri::root(true).'/media/'._JOOM_OPTION.'/css';
+$path_js = Uri::root(true).'/media/'._JOOM_OPTION.'/js';
 
 HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 $wa = $this->document->getWebAssetManager();
-$wa->registerAndUseStyle('com_joomgallery.uppy', 'https://releases.transloadit.com/uppy/'.$uppy_version.'/uppy.min.css');
+$wa->registerAndUseStyle('com_joomgallery.uppy', _JOOM_OPTION.'/uppy-'.$uppy_version.'.min.css');
+$wa->registerAndUseScript('com_joomgallery.uppy', _JOOM_OPTION.'/uppy-'.$uppy_version.'.js', [], ['type' => 'module', 'defer' => true]);
 $wa->useScript('keepalive')
 	 ->useScript('form.validate')
    ->useStyle('com_joomgallery.admin');
+
 HTMLHelper::_('bootstrap.tooltip');
 
 $app = Factory::getApplication();
@@ -35,6 +38,17 @@ $app = Factory::getApplication();
 $isModal = $app->input->get('layout') === 'modal';
 $layout  = $isModal ? 'modal' : 'edit';
 $tmpl    = $isModal || $app->input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=component' : '';
+
+// Add language strings to JavaScript
+Text::script('COM_JOOMGALLERY_DEBUG_INFORMATION');
+Text::script('JCLOSE');
+
+// Add variables to JavaScript
+$js_vars = new stdClass();
+$js_vars->maxFileSize = 262144000;
+$js_vars->TUSlocation = $this->item->tus_location;
+
+$wa->addInlineScript('window.uppyVars = JSON.parse(\''. json_encode($js_vars) . '\');', ['position' => 'before'], [], ['com_joomgallery.uppy']);
 ?>
 
 <div class="jg jg-upload">
@@ -86,125 +100,5 @@ $tmpl    = $isModal || $app->input->get('tmpl', '', 'cmd') === 'component' ? '&t
     <input type="hidden" name="task" value=""/>
     <?php echo HTMLHelper::_('form.token'); ?>
   </form>
-
   <div id="popup-area"></div>
 </div>
-
-<script type="module">
-  import { Uppy, Dashboard, Tus } from "https://releases.transloadit.com/uppy/<?php echo $uppy_version;?>/uppy.min.mjs"
-
-  let uppy = new Uppy({
-    autoProceed: false,
-    restrictions: {
-      maxFileSize: 262144000, // 250MB
-      allowedFileTypes: ['image/*', 'video/*', 'audio/*', 'text/*', 'application/*'],
-    }
-  });
-
-  if(uppy != null)
-  {
-    document.getElementById('drag-drop-area').innerHTML = '';
-  }
-
-  uppy.use(Dashboard, {
-    inline: true,
-    target: '#drag-drop-area',
-    showProgressDetails: true,
-    metaFields: [
-      { id: 'imgtitle', name: 'Name', placeholder: 'file name'},
-      { id: 'imgtext', name: 'Description', placeholder: 'description of the file' },
-      { id: 'author', name: 'Author', placeholder: 'author of the file' }
-    ],
-  });
-
-  uppy.use(Tus, {
-    endpoint: "<?php echo $this->item->tus_location; ?>",
-    retryDelays: [0, 1000, 3000, 5000],
-    allowedMetaFields: null,
-  })
-
-  let getUuid = function(uploadURL) {
-    let query = uploadURL.split('?')[1];
-    let queryArray = query.split('&');
-
-    for (let i = 0; i < queryArray.length; ++i) {
-      if(queryArray[i].includes('uuid')) {
-        return queryArray[i].replace('uuid=','');
-      }
-    }
-
-    return '';
-  }
-
-  let createPopup = function(id, content) {
-    let html =    '<div class="joomla-modal modal fade" id="modal'+id+'" tabindex="-1" aria-labelledby="modal'+id+'Label" aria-hidden="true">';
-    html = html +   '<div class="modal-dialog modal-lg">';
-    html = html +      '<div class="modal-content">';
-    html = html +           '<div class="modal-header">';
-    html = html +               '<h3 class="modal-title" id="modal'+id+'Label"><?php echo Text::_('COM_JOOMGALLERY_DEBUG_INFORMATION');?></h3>';
-    html = html +               '<button type="button" class="btn-close novalidate" data-bs-dismiss="modal" aria-label="<?php echo Text::_('JCLOSE');?>"></button>';
-    html = html +           '</div>';
-    html = html +           '<div class="modal-body">';
-    html = html +               '<div id="'+id+'-ModalBody">'+content+'</div>';
-    html = html +           '</div>';
-    html = html +           '<div class="modal-footer">';
-    html = html +               '<button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick="event.preventDefault()" aria-label="<?php echo Text::_('JCLOSE');?>"><?php echo Text::_('JCLOSE');?></button>';
-    html = html +           '</div>';
-    html = html +      '</div>';
-    html = html +   '</div>';
-    html = html + '</div>';
-
-    return html;
-  }
-
-  uppy.on('upload-success', (file, response) => {
-    // send form data and add record to database
-    console.log(file.name, response.uploadURL)
-    let tmp = '';
-  })
-
-  uppy.on('complete', (result) => {
-    // add message for successful images
-    for (let index = 0; index < result.successful.length; ++index) {
-      let res = result.successful[index];
-      let uuid = getUuid(res.uploadURL);
-
-      // Add Button to upload form
-      let btn = document.createElement('div');
-      btn.innerHTML = '<button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#modal'+uuid+'"><?php echo Text::_('COM_JOOMGALLERY_DEBUG_INFORMATION');?></button>';
-      btn.classList.add('uppy-Dashboard-Item-debug-msg');
-      btn.classList.add('success');
-      document.getElementById('uppy_'+res.id).lastChild.firstChild.appendChild(btn);
-
-      // Add Popup
-      let div = document.createElement('div');
-      div.innerHTML = createPopup(uuid, 'Upload of file "'+res.name+'" using Uppy successful.<br />Upload-ID: '+uuid+'<br />Debug-Info will be added here...');
-      document.getElementById('popup-area').appendChild(div);
-
-      new bootstrap.Modal(document.getElementById('modal'+uuid));
-    }
-
-    // add message for failed images
-    for (let index = 0; index < result.failed.length; ++index) {
-      let res = result.failed[index];
-      let uuid = getUuid(res.uploadURL);
-
-      // Add Button to upload form
-      let btn = document.createElement('div');
-      btn.innerHTML = '<button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modal'+uuid+'"><?php echo Text::_('COM_JOOMGALLERY_DEBUG_INFORMATION');?></button>';
-      btn.classList.add('uppy-Dashboard-Item-debug-msg');
-      btn.classList.add('error');
-      document.getElementById('uppy_'+res.id).lastChild.firstChild.appendChild(btn);
-
-      // Add Popup
-      let div = document.createElement('div');
-      div.innerHTML = createPopup(uuid, 'Upload not successful.<br />Debug-Info flow. To be added...');
-      document.getElementById('popup-area').appendChild(div);
-
-      new bootstrap.Modal(document.getElementById('modal'+uuid));
-    }
-
-    console.log('successful files:', result.successful);
-    console.log('failed files:', result.failed);
-  });
-</script>
