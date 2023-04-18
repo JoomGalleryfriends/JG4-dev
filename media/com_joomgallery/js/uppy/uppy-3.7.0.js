de4df477efe7fc8365b256ec4362b87c1963f654
19leunam93 <tech.spuur@quickline.ch>
2023-04-18 22:46:17 +0200

    add fetch js

--- media/com_joomgallery/js/uppy/uppy-3.7.0.js
+++ media/com_joomgallery/js/uppy/uppy-3.7.0.js
@@ -2,6 +2,56 @@
 
 import { Uppy, Dashboard, Tus } from "./uppy-3.7.0.min.js"
 
+/**
+ * Ajax request of form data to start the upload
+ *
+ * @param  {Integer} formID    The id of the form element
+ * @param  {String}  uuid	     The id of the uploaded element
+ *
+ * @returns  {void}
+ */
+function uploadAjax(formID, destination, uuid) {
+
+  // catch form data
+  //-----------------
+  let formData = new FormData(document.getElementById(formID));
+  formData.append('uuid', uuid);
+  //formData.append('task', 'download');
+
+  let parameters = {
+    method: 'POST',
+    mode: 'same-origin',
+    cache: 'default',
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer-when-downgrade',
+    body: formData,
+  };
+
+  async function postData(url, parameters) {
+    const response = await fetch(url, parameters);
+    if (!response.ok) {
+        // on network error
+        console.log("Network-Error: " + response.status + ", " + response.statusText);
+    }
+    else
+    {
+      // on success
+      return response.json();
+    }
+  }
+
+  postData(destination, parameters)
+    .then(res => {
+      let link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      document.body.appendChild(link);
+      link.download = fileName + '.' + val;
+      link.style = "display: none";
+      link.click();
+      document.body.removeChild(link);
+    });
+}
+
 var callback = function() {
   // document ready function
   
@@ -33,7 +83,7 @@ var callback = function() {
     endpoint: window.uppyVars.TUSlocation,
     retryDelays: [0, 1000, 3000, 5000],
     allowedMetaFields: null,
-  })
+  });
 
   let getUuid = function(uploadURL) {
     let query = uploadURL.split('?')[1];
@@ -69,11 +119,20 @@ var callback = function() {
     return html;
   }
 
+  let createBtn = function(result, type) {
+    // Add Button to upload form
+    let btn = document.createElement('div');
+    btn.innerHTML = '<button type="button" class="btn btn-'+type+' btn-sm" data-bs-toggle="modal" data-bs-target="#modal'+uuid+'">'+Joomla.JText._("COM_JOOMGALLERY_DEBUG_INFORMATION")+'</button>';
+    btn.classList.add('uppy-Dashboard-Item-debug-msg');
+    btn.classList.add('success');
+    document.getElementById('uppy_'+result.id).lastChild.firstChild.appendChild(btn);
+  }
+
   uppy.on('upload-success', (file, response) => {
     // send form data and add record to database
     console.log(file.name, response.uploadURL)
     let tmp = '';
-  })
+  });
 
   uppy.on('complete', (result) => {
     // add message for successful images
@@ -82,11 +141,7 @@ var callback = function() {
       let uuid = getUuid(res.uploadURL);
 
       // Add Button to upload form
-      let btn = document.createElement('div');
-      btn.innerHTML = '<button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#modal'+uuid+'">'+Joomla.JText._("COM_JOOMGALLERY_DEBUG_INFORMATION")+'</button>';
-      btn.classList.add('uppy-Dashboard-Item-debug-msg');
-      btn.classList.add('success');
-      document.getElementById('uppy_'+res.id).lastChild.firstChild.appendChild(btn);
+      createBtn(res, 'success');
 
       // Add Popup
       let div = document.createElement('div');
@@ -102,11 +157,7 @@ var callback = function() {
       let uuid = getUuid(res.uploadURL);
 
       // Add Button to upload form
-      let btn = document.createElement('div');
-      btn.innerHTML = '<button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modal'+uuid+'">'+Joomla.JText._("COM_JOOMGALLERY_DEBUG_INFORMATION")+'</button>';
-      btn.classList.add('uppy-Dashboard-Item-debug-msg');
-      btn.classList.add('error');
-      document.getElementById('uppy_'+res.id).lastChild.firstChild.appendChild(btn);
+      createBtn(res, 'danger');
 
       // Add Popup
       let div = document.createElement('div');
@@ -115,9 +166,6 @@ var callback = function() {
 
       new bootstrap.Modal(document.getElementById('modal'+uuid));
     }
-
-    console.log('successful files:', result.successful);
-    console.log('failed files:', result.failed);
   });
 
 }; //end callback
