f3806842d42d9880d02aa5bb3b3a9b92e09939e0
19leunam93 <tech.spuur@quickline.ch>
2023-06-19 20:26:11 +0200

    add ending delimiter

--- script.php
+++ script.php
@@ -125,7 +125,7 @@ class com_joomgalleryInstallerScript extends InstallerScript
 
     // rename old JoomGallery tables (JGv1-3)
     $jgtables = $this->detectJGtables();
-    if($jgtables && ($type == 'install' || ($type == 'update' && preg_match('^([1-3]\.)(\d+\.)(\d+)*(.+)', $this->act_code))))
+    if($jgtables && ($type == 'install' || ($type == 'update' && preg_match('/^([1-3]\.)(\d+\.)(\d+)*(.+)/', $this->act_code))))
     {
       $db = Factory::getDbo();
       foreach($jgtables as $oldTable)
