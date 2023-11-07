46c4001d33ba57fa04dce759b0e7002d9e47d234
19leunam93 <tech.spuur@quickline.ch>
2023-11-07 22:07:07 +0100

    border style: active migration

--- administrator/com_joomgallery/tmpl/migration/default.php
+++ administrator/com_joomgallery/tmpl/migration/default.php
@@ -29,22 +29,31 @@ $wa->useStyle('com_joomgallery.admin')
 
   <?php foreach ($this->scripts as $name => $script) : ?>
     <?php
-      $openMigrations = $this->openMigrations[$name] ? true : false;
+      $openMigrations    = false;
+      $openMigrationsIDs = '';
+
+      if(key_exists($name, $this->openMigrations))
+      {
+        $openMigrations    = true;
+        $openMigrationsIDs = $this->openMigrations[$name]->id;
+      }
     ?>
     <form action="<?php echo Route::_('index.php?option=com_joomgallery&view=migration'); ?>" method="post" name="adminForm" id="adminForm">
       <div class="row align-items-start">
         <div class="col-md-12">
-          <div class="card">
+          <div class="card<?php echo $openMigrations ? ' border active' : ''; ?>">
             <h3 class="card-header"><?php echo Text::_('FILES_JOOMGALLERY_MIGRATION_'.strtoupper($name).'_TITLE'); ?></h3>
             <div class="card-body row">
               <div class="col-2">
                 <img src="<?php echo $script['img']; ?>" alt="<?php echo $name; ?> logo">
               </div>
               <div id="formInputContainer" class="col-10">
+                <?php if($openMigrations) : ?>
+                  <div>
+                    <span class="badge bg-success"><?php echo Text::_('COM_JOOMGALLERY_SERVICE_MIGRATION_ACTIVE'); ?></span>
+                  </div><br />
+                <?php endif; ?>
                 <p><?php echo Text::_('FILES_JOOMGALLERY_MIGRATION_'.strtoupper($name).'_DESC'); ?></p>
-                <joomla-button id="migration-start" task="display">
-                  <button class="btn btn-primary <?php echo $openMigrations ? ' disabled' : ''; ?>" <?php echo $openMigrations ? 'disabled' : ''; ?> data-submit-task="display" type="button"><?php echo Text::_('COM_JOOMGALLERY_MIGRATION_START_SCRIPT'); ?></button>
-                </joomla-button>
                 <?php if($openMigrations) : ?>
                   <joomla-button id="migration-resume" task="migration.resume">
                     <button class="btn btn-primary" data-submit-task="migration.resume" type="button"><?php echo Text::_('COM_JOOMGALLERY_RESUME'); ?></button>
@@ -52,7 +61,11 @@ $wa->useStyle('com_joomgallery.admin')
                   <joomla-button id="migration-abort" task="migration.delete">
                     <button class="btn btn-primary" data-submit-task="migration.delete" type="button"><?php echo Text::_('COM_JOOMGALLERY_MIGRATION_ABORT_MIGRATION'); ?></button>
                   </joomla-button>
-                  <input type="hidden" name="cid[]" value="<?php echo $this->openMigrations[$name]->id; ?>"/>
+                  <input type="hidden" name="cid[]" value="<?php echo $openMigrationsIDs; ?>"/>
+                <?php else: ?>
+                  <joomla-button id="migration-start" task="display">
+                    <button class="btn btn-primary <?php echo $openMigrations ? ' disabled' : ''; ?>" <?php echo $openMigrations ? 'disabled' : ''; ?> data-submit-task="display" type="button"><?php echo Text::_('COM_JOOMGALLERY_MIGRATION_START_SCRIPT'); ?></button>
+                  </joomla-button>
                 <?php endif; ?>
                 <input type="hidden" name="layout" value="step1"/>
                 <input type="hidden" name="script" value="<?php echo $name; ?>"/>
