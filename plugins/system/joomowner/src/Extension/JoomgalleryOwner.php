41d2872a4b03f93c8924bc59fca6c471a2fa671d
Elfangor <tech.spuur@quickline.ch>
2024-09-17 08:12:57 +0200

    workaround JoomOwner plugin

--- plugins/system/joomowner/src/Extension/JoomgalleryOwner.php
+++ plugins/system/joomowner/src/Extension/JoomgalleryOwner.php
@@ -114,6 +114,13 @@ final class JoomgalleryOwner extends CMSPlugin implements SubscriberInterface
           $fieldname = 'cmsuser';
         }
 
+        $helperClass = '\\Joomgallery\\Component\\Joomgallery\\Administrator\\Helper\\JoomHelper';
+        if(!\class_exists($helperClass))
+        {
+          $helper_path = JPATH_ADMINISTRATOR.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_joomgallery'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Helper'.DIRECTORY_SEPARATOR.'JoomHelper.php';
+          require_once $helper_path;
+        }
+
         $this->tables[$name] = array( 'sing_name' => $name,
                                       'pl_name'   => $value['pl_name'],
                                       'tablename' => JoomHelper::getTableName($name),
