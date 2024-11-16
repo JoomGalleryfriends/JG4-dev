a531d3a178ee4fe9825fc178ddb6aa3895bcd515
19leunam93 <tech.spuur@quickline.ch>
2024-11-16 14:21:21 +0100

    fix issue when migrating 'votes'

--- administrator/com_joomgallery/sql/updates/mysql/4.0.0.sql
+++ administrator/com_joomgallery/sql/updates/mysql/4.0.0.sql
@@ -384,7 +384,7 @@ CREATE TABLE IF NOT EXISTS `#__joomgallery_votes` (
 `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 `imgid` INT(11) UNSIGNED NOT NULL DEFAULT 0,
 `score` INT(11) NOT NULL DEFAULT 0,
-`identication` INT(11) NOT NULL DEFAULT 0,
+`identication` VARCHAR(255) NULL DEFAULT "",
 `created_time` datetime NOT NULL,
 `created_by` INT(11) UNSIGNED NOT NULL DEFAULT 0,
 PRIMARY KEY (`id`),
