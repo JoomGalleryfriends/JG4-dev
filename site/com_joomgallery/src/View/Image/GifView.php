1ba9bfe4b2f896dc0f205444ff9eed381c862121
19leunam93 <tech.spuur@quickline.ch>
2024-11-18 08:46:10 +0100

    Add gif view

---
+++ site/com_joomgallery/src/View/Image/GifView.php
@@ -0,0 +1,25 @@
+<?php
+/**
+******************************************************************************************
+**   @version    4.0.0-dev                                                              **
+**   @package    com_joomgallery                                                        **
+**   @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>                 **
+**   @copyright  2008 - 2023  JoomGallery::ProjectTeam                                  **
+**   @license    GNU General Public License version 3 or later                          **
+*****************************************************************************************/
+
+namespace Joomgallery\Component\Joomgallery\Site\View\Image;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Raw view class for a single Image.
+ * 
+ * @package JoomGallery
+ * @since   4.0.0
+ */
+class GifView extends RawView
+{
+  // Use the RawView class
+}
