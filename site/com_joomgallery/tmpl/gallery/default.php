ac806c4f978d3c87f701d1a16302f9d5c160e30c
Peter Reill <43771555+reilldesign@users.noreply.github.com>
2024-12-05 15:01:06 +0100

    Update default.php

--- site/com_joomgallery/tmpl/gallery/default.php
+++ site/com_joomgallery/tmpl/gallery/default.php
@@ -11,9 +11,20 @@
 // No direct access
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Session\Session;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomgallery\Component\Joomgallery\Administrator\Helper\JoomHelper;
+
+// image params
+$category_class   = $this->params['configs']->get('jg_category_view_class', 'column', 'STRING');
+$num_columns      = $this->params['configs']->get('jg_category_view_num_columns', 3, 'INT');
+
+$image_link       = $this->params['configs']->get('jg_category_view_image_link', 'defaultview', 'STRING');
+$title_link       = $this->params['configs']->get('jg_category_view_title_link', 'defaultview', 'STRING');
 
 // Import CSS & JS
 $wa = $this->document->getWebAssetManager();
@@ -21,19 +32,56 @@ $wa->useStyle('com_joomgallery.site');
 $wa->useStyle('com_joomgallery.jg-icon-font');
 ?>
 
-<div class="com-joomgallery-gallery">
+<?php // Gallery ?>
+<?php if(count($this->item->images->items) > 0) : ?>
+  <div class="jg-gallery<?php echo ' ' . $category_class; ?>" itemscope="" itemtype="https://schema.org/ImageGallery">
+
   <?php if ($this->params['menu']->get('show_page_heading')) : ?>
     <div class="page-header">
       <h1> <?php echo $this->escape($this->params['menu']->get('page_heading')); ?> </h1>
     </div>
   <?php endif; ?>
 
-  <p>This is the default JoomGallery-Page. In the future, this will become a beautiful image wall page with search bar and filters. A perfect entry point to your gallery.</p>
-  <ul>
-    <li><a href="<?php echo Route::_('index.php?option=com_joomgallery&view=category&id=1'); ?>">Category View</a></li>
-    <?php if($this->params['configs']->get('jg_userspace', 1, 'int') == 1): ?>
-      <li><a href="<?php echo Route::_('index.php?option=com_joomgallery&view=categories'); ?>">Categories List View</a></li>
-      <li><a href="<?php echo Route::_('index.php?option=com_joomgallery&view=images'); ?>">Images List View</a></li>
-    <?php endif; ?>
-  </ul>
+    <div id="lightgallery-<?php echo $this->item->id; ?>" class="jg-images <?php echo $category_class; ?>-<?php echo $num_columns; ?> jg-category" data-masonry="{ pollDuration: 175 }">
+
+
+  <?php foreach ($this->item->images->items as $key => $item) : ?>
+
+        <div class="jg-image">
+          <div class="jg-image-thumbnail boxed">
+
+            <?php // if($image_link == 'defaultview') : ?>
+              <a href="<?php echo Route::_('index.php?option=com_joomgallery&view=image&id='.(int) $item->id); ?>">
+                <img src="<?php echo JoomHelper::getImg($item, 'thumbnail'); ?>" class="jg-image-thumb" alt="<?php echo $item->title; ?>" itemprop="image" itemscope="" itemtype="https://schema.org/image"<?php if ( $category_class != 'justified') : ?> loading="lazy"<?php endif; ?>>
+              </a>
+                  <div class="jg-image-caption <?php echo $caption_align; ?>">
+                  <a href="<?php echo Route::_('index.php?option=com_joomgallery&view=image&id='.(int) $item->id); ?>">
+                    <?php echo $this->escape($item->title); ?>
+                  </a>
+                  </div>
+
+                  <div><?php echo $this->escape($item->cattitle); ?></div>
+
+            <?php // endif; ?>
+
+       </div>
+       </div>
+
+
+  <?php endforeach; ?>
+
+    </div>
+
 </div>
+
+<?php endif; ?>
+
+<script>
+let images = document.getElementsByClassName('jg-image-thumb');
+for (let image of images) {
+  image.addEventListener('load', loadImg);
+}
+function loadImg () {
+  this.closest('.jg-image').classList.add('loaded');
+}
+</script>
